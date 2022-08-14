f8e6329f2bfc20eda9bb59bca839e8dc2b1e6961
ArchyScript <yungdansa@gmail.com>
2022-08-14 01:07:56 +0100

    ...

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@
 // import { Splide } from '@splidejs/splide'
 
 const nav_links_container = document.getElementById('nav_links_container')
+const nav_link_mobile = document.getElementById('nav_link_mobile')
 const celebs_container = document.getElementById('celebs_container')
 const nav_links = [
   {
@@ -31,67 +32,75 @@ const nav_links = [
 ]
 
 const celebs = [
-  {
-    name: '',
-    image: '',
-  },
+  // {
+  //   name: '',
+  //   image: '',
+  // },
   {
     name: 'Lupita Nyong’o',
-    image: './img/lupita.png',
+    image: './src/img/lupita.png',
   },
   {
     name: 'Timini Egbuson',
-    image: './img/timini.png',
+    image: './src/img/timini.png',
   },
   {
     name: 'Abayomi Alvin',
-    image: './img/aby.png',
+    image: './src/img/aby.png',
   },
   {
     name: ' Adebukola Oladipupo',
-    image: './img/bukky.png',
+    image: './src/img/bukky.png',
   },
   {
     name: 'Adesua Etomi',
-    image: './img/adesua.png',
+    image: './src/img/adesua.png',
   },
   {
     name: 'Dorcas Shola Fapson',
-    image: './img/dorcas.png',
+    image: './src/img/dorcas.png',
   },
   {
     name: 'Jemima Osunde',
-    image: './img/jemina.png',
+    image: './src/img/jemina.png',
   },
   {
     name: 'Olumide Owuru',
-    image: './img/olumide.png',
+    image: './src/img/olumide.png',
   },
   {
     name: 'Maria Okanrende',
-    image: './img/maria.png',
+    image: './src/img/maria.png',
   },
   {
     name: 'Rahma Sadau',
-    image: './img/rhama.png',
+    image: './src/img/rhama.png',
   },
   {
     name: 'Emmanuel Ikibese',
-    image: './img/emma.png',
+    image: './src/img/emma.png',
   },
 ]
 
 //
 const handleNavLinks = async () => {
   nav_links_container.innerHTML = ''
+  nav_link_mobile.innerHTML = ''
 
   nav_links.forEach((nav_link) => {
     nav_links_container.innerHTML += `
-       <li class="text-gray-200  hover:text-white hover:underline px-1 sm:px-2  font-semibold text-sm xl:text-lg">
+       <li class="text-gray-200  hover:text-white hover:underline px-1 sm:px-2  font-medium lg:font-semibold text-sm xl:text-lg">
             <a href="#" class="">
                 ${nav_link.title}
             </a>
         </li>
+    `
+    nav_link_mobile.innerHTML += `
+        <div class="text-purple-200  hover:text-white  py-1 sm:py-2 px-1 sm:px-2 font-semibold text-xl sm:text-2xl">
+            <a href="#" class="text-2xl">
+                ${nav_link.title}
+            </a>
+        </div>
     `
   })
 }
@@ -101,12 +110,13 @@ const displayCelebs = () => {
 
   celebs.forEach((celeb) => {
     celebs_container.innerHTML += `
-        <div class="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 w-full mb-4">
+        <div class="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 w-full  mb-1 sm:mb-2 lg:mb-4">
             <div class="relative  w-full">
-                <div class="flex-shrink-0 cursor-pointer">
+                <div class="flex-shrink-0 cursor-pointer  flex justify-center items-center">
                     <img src="${celeb.image}"   />
                 </div>
-                <p class="text-white text-center cursor-pointer uppercase font-medium md:font-semibold text-xl flex-wrap mt-4">
+
+                <p class="text-purple-100 text-center cursor-pointer uppercase font-medium md:font-semibold text-base lg:text-lg flex-wrap mt-4">
                     ${celeb.name}
                 </p>
             </div>
@@ -114,9 +124,30 @@ const displayCelebs = () => {
     `
   })
 }
+
+const handleScroll = () => {
+  if (window.pageYOffset > 0) {
+    // user is scrolled
+    // if (scrollShadowBoolean.value) scrollShadowBoolean.value = false
+  } else {
+    // user is at top of page
+    // if (!scrollShadowBoolean.value) scrollShadowBoolean.value = true
+  }
+}
+
+document.getElementById('close_menu_btn').onclick = () => toggleMenuBar()
+document.getElementById('open_menu_btn').onclick = () => toggleMenuBar()
+const modal = document.getElementById('modal')
+
+const toggleMenuBar = () => {
+  // modal.classList.toggle('fixed')
+  modal.classList.toggle('hidden')
+}
+
 //
 //
 window.onload = function () {
+  modal.classList.add('hidden')
   handleNavLinks()
   displayCelebs()
 }
